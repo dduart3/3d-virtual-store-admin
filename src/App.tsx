4f68f91a6d775185146bce451566723607f310e8
antocalini <xiaocalini@gmail.com>
2025-03-15 13:59:34 -0400

    removed unused imports and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import { useMemo } from "react";
-import { KeyboardControls, KeyboardControlsEntry } from "@react-three/drei";
+
 import { ToastProvider } from "./shared/context/ToastContext";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClient } from "./lib/queryClient";
@@ -7,38 +6,19 @@ import { AuthProvider } from "./modules/auth/context/AuthProvider";
 import { RouterProvider } from "@tanstack/react-router";
 import { router } from "./router";
 
-enum Controls {
-  forward = "forward",
-  backward = "backward",
-  left = "left",
-  right = "right",
-  jump = "jump",
-  run = "run",
-}
 
 function App() {
-  const map = useMemo<KeyboardControlsEntry<Controls>[]>(
-    () => [
-      { name: Controls.forward, keys: ["ArrowUp", "KeyW"] },
-      { name: Controls.backward, keys: ["ArrowDown", "KeyS"] },
-      { name: Controls.left, keys: ["ArrowLeft", "KeyA"] },
-      { name: Controls.right, keys: ["ArrowRight", "KeyD"] },
-      { name: Controls.jump, keys: ["Space"] },
-      { name: Controls.run, keys: ["ShiftLeft", "ShiftRight"] },
-    ],
-    []
-  );
   return (
     <QueryClientProvider client={queryClient}>
       <AuthProvider>
         <ToastProvider>
-          <KeyboardControls map={map}>
-            <RouterProvider router={router} />
-          </KeyboardControls>
+          <RouterProvider router={router} />
         </ToastProvider>
       </AuthProvider>
-    </QueryClientProvider>
+    </QueryClientProvider >
   );
 }
 
 export default App;
+
+
